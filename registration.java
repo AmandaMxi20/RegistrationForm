760f678f3311c0c3106496c55e1a8f8ac3c6495d
Amanda Mxi <62343244+AmandaMxi20@users.noreply.github.com>
2020-07-20 01:43:59 +0200

    Update registration.java

--- registration.java
+++ registration.java
@@ -1,1 +1,16 @@
+package myClass;
+import java.util.Scanner;
 
+public class myClass {
+public static void main(String[]args) {
+	Scanner user_input = new Scanner(System.in);
+	String userName;
+	System.out.print("Enter your first name: ");
+	userName = user_input.next();
+	
+	int ContactDetails;
+	System.out.print("Please enter your contact details:");
+	ContactDetails =user_input.nextInt();
+	
+}
+}
