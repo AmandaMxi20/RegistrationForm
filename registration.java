ccb14d1091ba8990812bb98d4bcc033c70c654a6
Amanda Mxi <62343244+AmandaMxi20@users.noreply.github.com>
2020-07-20 01:51:16 +0200

    Update registration.java

--- registration.java
+++ registration.java
@@ -1,5 +1,8 @@
 package myClass;
 import java.util.Scanner;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class myClass {
 public static void main(String[]args) {
@@ -12,5 +15,18 @@ public static void main(String[]args) {
 	System.out.print("Please enter your contact details:");
 	ContactDetails =user_input.nextInt();
 	
+	Date now = new Date();
+        System.out.println(now);
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd MM yyyy");
+        dateFormat.format(now);
+        System.out.println(now);
+
+        Date birth = dateFormat.parse("");
+        System.out.println(birth);
+	
 }
+
+        
+   
 }
