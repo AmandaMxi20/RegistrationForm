d60527b94ba72fa3df40745b023c843ecca9f204
Amanda Mxi <62343244+AmandaMxi20@users.noreply.github.com>
2020-07-20 02:01:18 +0200

    Update registration.java

--- registration.java
+++ registration.java
@@ -9,12 +9,14 @@ public static void main(String[]args) {
 	Scanner user_input = new Scanner(System.in);
 	String userName;
 	System.out.print("Enter your first name: ");
-	userName = user_input.next();
+	userName = user_input.nextLine();
 	
+	//contact details 
 	int ContactDetails;
 	System.out.print("Please enter your contact details:");
 	ContactDetails =user_input.nextInt();
 	
+	//Date of birth
 	Date now = new Date();
         System.out.println(now);
 
@@ -24,6 +26,12 @@ public static void main(String[]args) {
 
         Date birth = dateFormat.parse("");
         System.out.println(birth);
+	
+	
+	//Address
+	System.out.println("Please enter your physical address:")
+		address.in.nextLine;
+	System.out.print(address);
 	
 }
 
